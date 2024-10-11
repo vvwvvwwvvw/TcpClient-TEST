0c65ad77cf6b6ca937a66ac8c4206b19ce7bb291
남정욱 <wjddnr3304@gmail.com>
2024-10-11 15:19:53 +0900

    초기 커밋.

---
+++ TcpClient TEST/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpClient_TEST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // (1) IP 주소와 포트를 지정하고 TCP 연결
+            TcpClient tcpClient = new TcpClient("127.0.0.1", 8000);
+            // TcpClient tcpClient = new TcpClient tcpClient("localhost", 8000);
+            string msg = "Hello World";
+            byte[] buffer = Encoding.ASCII.GetBytes(msg);
+
+            // (2) 
+        }
+    }
+}
